26557f54193b282e30f23533e3adf82557e309d4
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2022-03-20 22:50:00 +0530

    update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 //     console.log(e.target.files[0]);
 //     img.src = URL.createObjectURL(e.target.files[0]);
 // })
-//polyfill for findIndex
+
+// polyfill for findIndex
 if(!Array.prototype.findIndex) {
     Object.defineProperty(Array.prototype,"findIndex", {
         value: function (cb) {
@@ -17,8 +18,8 @@ if(!Array.prototype.findIndex) {
             var requiredArray = Object(this), len = requiredArray.length, k=0;
             while(k<len) {
                 var value = requiredArray[k];
-                if(cb(undefined, value, requiredArray)) {
-                    return l
+                if(cb.call(undefined, value, k, requiredArray)) {
+                    return k;
                 }
                 k++
             }
@@ -31,8 +32,21 @@ if(!Array.prototype.findIndex) {
 if(!Array.prototype.find) {
     Object.defineProperty(Array.prototype, "find", {
         value : function (cb) {
-            
-        }
-
+            if(typeof cb !== "function") {
+                throw new TypeError("Callback function must be a function")
+            }
+            if(this === null) {
+                throw new TypeError("find is not called on an Array")
+            }
+            var requiredArray = Object(this), len = requiredArray.length, k=0;
+            while(k<len) {
+                var value = requiredArray[k];
+                if(cb.call(undefined, value, requiredArray)) {
+                    return value;
+                }
+                k++
+            }
+            return undefined;
+        },
     })
 }
